3882434d79ea29dec8ad8f098f58352e70360dd6
MEDOYED <mmax26072005@gmail.com>
2024-06-25 10:19:08 +0200

    added and customized .prettierrc

--- calculator/index.js
+++ calculator/index.js
@@ -1,24 +1,24 @@
-const input = document.querySelector(".calculator__input");
-const btnItem = document.querySelector(".button__item");
-const clear = document.querySelector(".button__clear");
-const changeSign = document.querySelector(".button__change-sign");
-const percent = document.querySelector(".button__percent");
-const divide = document.querySelector(".button__divide");
-const one = document.querySelector(".button__one");
-const two = document.querySelector(".button__two");
-const three = document.querySelector(".button__three");
-const multiply = document.querySelector(".button__multiply");
-const four = document.querySelector(".button__four");
-const five = document.querySelector(".button__five");
-const six = document.querySelector(".button__six");
-const minus = document.querySelector(".button__minus");
-const seven = document.querySelector(".button__seven");
-const eight = document.querySelector(".button__eight");
-const nine = document.querySelector(".button__nine");
-const plus = document.querySelector(".button__plus");
-const zero = document.querySelector(".button__zero");
-const comma = document.querySelector(".button__comma");
-const equal = document.querySelector(".button__equal");
+const input = document.querySelector('.calculator__input');
+const btnItem = document.querySelector('.button__item');
+const clear = document.querySelector('.button__clear');
+const changeSign = document.querySelector('.button__change-sign');
+const percent = document.querySelector('.button__percent');
+const divide = document.querySelector('.button__divide');
+const one = document.querySelector('.button__one');
+const two = document.querySelector('.button__two');
+const three = document.querySelector('.button__three');
+const multiply = document.querySelector('.button__multiply');
+const four = document.querySelector('.button__four');
+const five = document.querySelector('.button__five');
+const six = document.querySelector('.button__six');
+const minus = document.querySelector('.button__minus');
+const seven = document.querySelector('.button__seven');
+const eight = document.querySelector('.button__eight');
+const nine = document.querySelector('.button__nine');
+const plus = document.querySelector('.button__plus');
+const zero = document.querySelector('.button__zero');
+const comma = document.querySelector('.button__comma');
+const equal = document.querySelector('.button__equal');
 
 input.textContent = 0;
 one.textContent = 1;
@@ -31,7 +31,7 @@ seven.textContent = 7;
 eight.textContent = 8;
 nine.textContent = 9;
 zero.textContent = 0;
-percent.textContent = "%";
+percent.textContent = '%';
 // multiply.textContent = '/';
 
 const writeInInput = function (e) {
@@ -58,13 +58,13 @@ const buttons = [
   percent,
 ];
 
-buttons.forEach((button) => {
-  button.addEventListener("click", () => {
+buttons.forEach(button => {
+  button.addEventListener('click', () => {
     writeInInput(button);
   });
 });
 
-equal.addEventListener("click", () => {
+equal.addEventListener('click', () => {
   input.textContent = eval(input.textContent);
 });
 
@@ -77,34 +77,34 @@ const writeInInputSymbol = function (symbol) {
 };
 
 //   multyply
-multiply.addEventListener("click", () => {
+multiply.addEventListener('click', () => {
   if (input.textContent == 0) {
-    input.textContent = "*";
+    input.textContent = '*';
   } else {
-    input.textContent = input.textContent + "*";
+    input.textContent = input.textContent + '*';
   }
 });
 
 // devide
-divide.addEventListener("click", () => {
+divide.addEventListener('click', () => {
   if (input.textContent == 0) {
-    input.textContent = "/";
+    input.textContent = '/';
   } else {
-    input.textContent = input.textContent + "/";
+    input.textContent = input.textContent + '/';
   }
 });
 
 // comma
-comma.addEventListener("click", () => {
+comma.addEventListener('click', () => {
   if (input.textContent == 0) {
-    input.textContent = ".";
+    input.textContent = '.';
   } else {
-    input.textContent = input.textContent + ".";
+    input.textContent = input.textContent + '.';
   }
 });
 
 // AC / buttonClear
-clear.addEventListener("click", () => {
+clear.addEventListener('click', () => {
   input.textContent = 0;
 });
 
@@ -115,6 +115,6 @@ changeSign.addEventListener('click', () => {
   } else if (input.textContent > 0) {
     input.textContent = '-' + input.textContent;
   } else {
-    input.textContent =  input.textContent * (-1);
+    input.textContent = input.textContent * -1;
   }
 });
