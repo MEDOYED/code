334c1540e3e61ba810a202e8612dd5d2bd0debc9
MEDOYED <mmax26072005@gmail.com>
2024-06-26 19:29:08 +0200

    scope | modified

--- scope/script.js
+++ scope/script.js
@@ -1,15 +1,31 @@
-'use strict';
+// 'use strict';
 
 function getAge(birthYear) {
   const age = 2024 - birthYear;
 
   function printAge() {
-    const info = `${firstName} is ${age}, born in ${birthYear}`;
+    let info = `${firstName} is ${age}, born in ${birthYear}`;
     console.log(info);
+
+    if (age >= 18) {
+      var isAdult = true;
+      const firstName = 'John';
+      const adult = `${firstName} is adult`;
+      console.log(adult);
+
+      function sum(a, b) {
+        return a + b;
+      }
+      console.log(sum(1, 3));
+
+      info = 'New info';
+    }
+    console.log(info);
+    console.log(isAdult);
   }
 
   printAge();
-
+  // console.log(isAdult);   ReferenceError:
   return age;
 }
 
