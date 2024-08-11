8108068b64dc0db3942f823c8a2d71eec7aec650
MEDOYED <mmax26072005@gmail.com>
2024-08-11 21:36:45 +0200

    arrays | theory

---
+++ arrays/theoty/script.js
@@ -0,0 +1,74 @@
+'use strict';
+
+// const currencies = new Map([
+//   ['USD', 'United States dollar'],
+//   ['EUR', 'Euro'],
+//   ['CNY', 'Chinese yuan'],
+// ]);
+
+// //////////////////////////////////////////////////////////////////////////////////
+
+// .slice()
+
+// console.log(letters.slice(2));
+// console.log(letters.slice(2, 5));
+// console.log(letters.slice(-3));
+
+// .splice()
+
+// letters.splice(1, 4);  // ['a', 'f']
+// letters.splice(1, 4);  // ['a', 'b']
+// letters.splice(1, 3);
+
+// let letters1 = ['a', 'b', 'c', 'd', 'e', 'f'];
+// let letters2 = ['g', 'h', 'i'];
+
+// // reverse()
+
+// // console.log(letters.reverse());
+
+// console.log(letters1.concat(letters2));
+
+// console.log(letters1);
+
+// for (const transaction of transactions) {
+//   if (transaction > 0) {
+//     console.log(`${transaction} was deposited`);
+//   } else {
+//     console.log(`${Math.abs(transaction)} was withdrew`);
+//   }
+// }
+
+// const transactions = [300, 250, -500, 5000, -750, -180, 50, 1400, -350, -120];
+
+// transactions.forEach(function (transaction, index) {
+//   if (transaction > 0) {
+//     console.log(`Transaction № ${index + 1}: ${transaction} was deposited`);
+//   } else {
+//     console.log(`Transaction № ${index + 1}: ${Math.abs(transaction)} was withdrew`);
+//   }
+// });
+
+// for each з Map і Set
+
+// Map
+
+const currencies = new Map([
+  ['USD', 'United States dollar'],
+  ['EUR', 'Euro'],
+  ['CNY', 'Chinese yuan'],
+]);
+
+currencies.forEach(function (value, key, map) {
+  console.log(key, value);
+});
+
+console.log('---------------------');
+
+// Set
+const uniqueCurrencies = new Set(['USD', 'EUR', 'CNY', 'CNY']);
+console.log(uniqueCurrencies);
+uniqueCurrencies.forEach(function (value, _, set) {
+  console.log(`${value}: ${value}`);
+  console.log(set);
+});
