a8082c483bac8ba706d6c4b9830ff92740c0c173
MEDOYED <mmax26072005@gmail.com>
2024-08-20 21:48:29 +0200

    arrays | .filter()

--- arrays/mapFilterReduce/index.js
+++ arrays/mapFilterReduce/index.js
@@ -1,26 +1,26 @@
 // Method map()
 
-const transactions = [500, 250, -300, 5000, -850, -110, -170, 1100];
+// const transactions = [500, 250, -300, 5000, -850, -110, -170, 1100];
 
-const usdToEuro = 0.86;
+// const usdToEuro = 0.86;
 
 // const transactionsEuro = transactions.map(function(trans) {
 //   return trans * usdToEuro;
 // })
 
-const transactionsEuro = transactions.map(trans => trans * usdToEuro);
+// const transactionsEuro = transactions.map(trans => trans * usdToEuro);
 
-console.log(transactions);
-console.log('==================');
-console.log(transactionsEuro);
+// console.log(transactions);
+// console.log('==================');
+// console.log(transactionsEuro);
 
-const transactionsEuro1 = [];
+// const transactionsEuro1 = [];
 
-for (const trans of transactions) {
-  transactionsEuro1.push(trans * usdToEuro);
-}
+// for (const trans of transactions) {
+//   transactionsEuro1.push(trans * usdToEuro);
+// }
 
-console.log(transactionsEuro1);
+// console.log(transactionsEuro1);
 
 // const transactionDescription = transactions.map((transaction, index, array) => {
 //   let description = '';
@@ -36,11 +36,49 @@ console.log(transactionsEuro1);
 //   return description;
 // });
 
-const transactionDescription = transactions.map(
-  (transaction, index, array) =>
-    `Transaction №${index + 1}: ${Math.abs(transaction)} was ${
-      transaction > 0 ? 'deposited' : 'withdrew'
-    }`,
-);
+// const transactionDescription = transactions.map(
+//   (transaction, index, array) =>
+//     `Transaction №${index + 1}: ${Math.abs(transaction)} was ${
+//       transaction > 0 ? 'deposited' : 'withdrew'
+//     }`,
+// );
 
-console.log(transactionDescription);
+// console.log(transactionDescription);
+
+// =========================================================================================================================
+// =========================================================================================================================
+// =========================================================================================================================
+
+Method .filter()
+
+const transactions = [500, 250, -300, 5000, -850, -110, -170, 1100];
+
+const withdrawals = transactions.filter(function (trans) {
+  return trans < 0;
+});
+
+console.log(withdrawals);
+
+const withdrawals1 = [];
+
+for (const trans of transactions) {
+  if (trans < 1) {
+    withdrawals1.push(trans);
+  }
+}
+
+console.log(withdrawals1);
+
+// const transactions = [500, 250, -300, 5000, -850, -110, -170, 1100];
+
+// const deposites = transactions.filter(function (trans) {
+//   return trans > 0;
+// });
+
+// console.log(deposites);
+
+const deposites = transactions.filter(trans => trans > 0);
+
+console.log(deposites);
+
+
