d39a3e7b2777d46e0205f01928494a807c3bced5
MEDOYED <mmax26072005@gmail.com>
2024-08-21 18:14:44 +0200

    arrays | task3mapFilterReduce udemy

--- arrays/task3mapFilterReduce/script.js
+++ arrays/task3mapFilterReduce/script.js
@@ -14,39 +14,55 @@ const catsAges2 = [1, 16, 12, 4, 5, 1, 3, 11, 7, 2];
 const catsAges1 = [7, 3, 2, 4, 1, 15, 8, 1, 9, 2];
 
 // 1. Рассчитывает человеческий возраст по следующей формуле: если возраст кошки <= 2 года, человеческий возраст = возраст кошки * 10. Если кошке больше 2 лет, человеческий возраст = возраст кошки * 7.
-const getHumanAge = function (arr) {
-  const humanAge = arr.map(function (i) {
-    if (i <= 2) {
-      return i * 10;
-    } else {
-      return i * 7;
-    }
-  });
-  return humanAge;
+// const getHumanAge = function (arr) {
+//   const humanAge = arr.map(function (i) {
+//     if (i <= 2) {
+//       return i * 10;
+//     } else {
+//       return i * 7;
+//     }
+//   });
+//   return humanAge;
+// };
+// console.log(getHumanAge(catsAges1));
+// console.log(getHumanAge(catsAges2));
+
+// // 2. Исключает всех кошек младше 18 человеческих лет.
+// const more18YearsCatsAges1 = getHumanAge(catsAges1).filter(function (i) {
+//   return i >= 18;
+// });
+
+// console.log(more18YearsCatsAges1);
+
+// const more18YearsCatsAges2 = getHumanAge(catsAges2).filter(function (i) {
+//   return i >= 18;
+// });
+
+// console.log(more18YearsCatsAges2);
+
+// // 3. Возвращает средний человеческий возраст для всех взрослых кошек.
+
+// const getAverageHumanAge = function(arr) {
+//   const total = arr.reduce(function(acc, item) {
+//     return acc + item
+//   }, 0);
+//   return total / arr.length;
+// }
+// console.log(getAverageHumanAge(more18YearsCatsAges1));
+// console.log(getAverageHumanAge(more18YearsCatsAges2));
+
+const getAverageHumanAge = function (catsAges) {
+  const humanAges = catsAges.map(catAge =>
+    catAge <= 2 ? catAge * 10 : catAge * 7,
+  );
+  const adultCats = humanAges.filter(humanAge => humanAge >= 18);
+  const averageHumanAge =
+    adultCats.reduce((acc, catAge) => acc + catAge, 0) / adultCats.length;
+  return averageHumanAge;
 };
-console.log(getHumanAge(catsAges1));
-console.log(getHumanAge(catsAges2));
 
-// 2. Исключает всех кошек младше 18 человеческих лет.
-const more18YearsCatsAges1 = getHumanAge(catsAges1).filter(function (i) {
-  return i >= 18;
-});
+const averageHumanAge1 = getAverageHumanAge([1, 16, 12, 4, 5, 1, 3, 11, 7, 2]);
+const averageHumanAge2 = getAverageHumanAge([7, 3, 2, 4, 1, 15, 8, 1, 9, 2]);
 
-console.log(more18YearsCatsAges1);
-
-const more18YearsCatsAges2 = getHumanAge(catsAges2).filter(function (i) {
-  return i >= 18;
-});
-
-console.log(more18YearsCatsAges2);
-
-// 3. Возвращает средний человеческий возраст для всех взрослых кошек.
-
-const getAverageHumanAge = function(arr) {
-  const total = arr.reduce(function(acc, item) {
-    return acc + item
-  }, 0);
-  return total / arr.length;
-}
-console.log(getAverageHumanAge(more18YearsCatsAges1));
-console.log(getAverageHumanAge(more18YearsCatsAges2));
+console.log(averageHumanAge1);
+console.log(averageHumanAge2);
