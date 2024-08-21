4921fcb8b278195e7b24145ea919a593155932f3
MEDOYED <mmax26072005@gmail.com>
2024-08-21 13:32:58 +0200

    simpleBank | displayTotal with chaining method

--- SimpleBank/script.js
+++ SimpleBank/script.js
@@ -118,3 +118,27 @@ const displayBalance = function (transactions) {
 };
 
 displayBalance(account1.transactions);
+
+const displayTotal = function (transactions) {
+  const depositesTotal = transactions
+    .filter(trans => trans > 0)
+    .reduce((acc, trans) => acc + trans, 0);
+  labelSumIn.textContent = `${depositesTotal}$`;
+
+  const withdrawalTotal = transactions
+    .filter(trans => trans < 0)
+    .reduce((acc, trans) => acc + trans, 0);
+  labelSumOut.textContent = `${withdrawalTotal}$`;
+
+  const interestTotal = transactions
+    .filter(trans => trans > 0)
+    .map(depos => (depos * 1.1) / 100)
+    .filter((interest, index, arr) => {
+      console.log(arr);
+      return interest >= 5
+    }) 
+    .reduce((acc, interest) => acc + interest, 0);
+  labelSumInterest.textContent = `${interestTotal}$`
+};
+
+displayTotal(account1.transactions);
