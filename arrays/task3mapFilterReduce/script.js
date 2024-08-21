79b00ea2e521b8a1463e5b641c95488e7d3cdea1
MEDOYED <mmax26072005@gmail.com>
2024-08-21 18:32:17 +0200

    arrays | task3mapFilterReduce

--- arrays/task3mapFilterReduce/script.js
+++ arrays/task3mapFilterReduce/script.js
@@ -10,8 +10,8 @@
 // 1: [7 , 3, 2, 4, 1, 15, 8, 1, 9, 2]
 // 2: [1, 16, 12, 4, 5, 1, 3, 11, 7, 2]
 
-const catsAges2 = [1, 16, 12, 4, 5, 1, 3, 11, 7, 2];
-const catsAges1 = [7, 3, 2, 4, 1, 15, 8, 1, 9, 2];
+// const catsAges2 = [1, 16, 12, 4, 5, 1, 3, 11, 7, 2];
+// const catsAges1 = [7, 3, 2, 4, 1, 15, 8, 1, 9, 2];
 
 // 1. Рассчитывает человеческий возраст по следующей формуле: если возраст кошки <= 2 года, человеческий возраст = возраст кошки * 10. Если кошке больше 2 лет, человеческий возраст = возраст кошки * 7.
 // const getHumanAge = function (arr) {
@@ -51,18 +51,34 @@ const catsAges1 = [7, 3, 2, 4, 1, 15, 8, 1, 9, 2];
 // console.log(getAverageHumanAge(more18YearsCatsAges1));
 // console.log(getAverageHumanAge(more18YearsCatsAges2));
 
-const getAverageHumanAge = function (catsAges) {
-  const humanAges = catsAges.map(catAge =>
-    catAge <= 2 ? catAge * 10 : catAge * 7,
-  );
-  const adultCats = humanAges.filter(humanAge => humanAge >= 18);
-  const averageHumanAge =
-    adultCats.reduce((acc, catAge) => acc + catAge, 0) / adultCats.length;
-  return averageHumanAge;
-};
+// const getAverageHumanAge = function (catsAges) {
+//   const humanAges = catsAges.map(catAge =>
+//     catAge <= 2 ? catAge * 10 : catAge * 7,
+//   );
+//   const adultCats = humanAges.filter(humanAge => humanAge >= 18);
+//   const averageHumanAge =
+//     adultCats.reduce((acc, catAge) => acc + catAge, 0) / adultCats.length;
+//   return averageHumanAge;
+// };
 
-const averageHumanAge1 = getAverageHumanAge([1, 16, 12, 4, 5, 1, 3, 11, 7, 2]);
-const averageHumanAge2 = getAverageHumanAge([7, 3, 2, 4, 1, 15, 8, 1, 9, 2]);
+// const averageHumanAge1 = getAverageHumanAge([1, 16, 12, 4, 5, 1, 3, 11, 7, 2]);
+// const averageHumanAge2 = getAverageHumanAge([7, 3, 2, 4, 1, 15, 8, 1, 9, 2]);
+
+// console.log(averageHumanAge1);
+// console.log(averageHumanAge2);
+
+// Перепишите функцию getAverageHumanAge из Задания 2, но на этот раз как стрелочную функцию и с использованием method chaining.
+// Тестовые данные:
+// 1: [7 , 3, 2, 4, 1, 15, 8, 1, 9, 2]
+// 2: [1, 16, 12, 4, 5, 1, 3, 11, 7, 2]
+
+const getAverageHumanAge = catsAges => catsAges
+    .map(catAge => (catAge <= 2 ? catAge * 10 : catAge * 7))
+    .filter(humanAge => humanAge >= 18)
+    .reduce((acc, catAge, index, arr) => acc + catAge / arr.length, 0);
+
+const averageHumanAge1 = getAverageHumanAge([7, 3, 2, 4, 1, 15, 8, 1, 9, 2]);
+const averageHumanAge2 = getAverageHumanAge([1, 16, 12, 4, 5, 1, 3, 11, 7, 2]);
 
 console.log(averageHumanAge1);
 console.log(averageHumanAge2);
