e557378527acd95eb133c113fe54a505f0f5304f
MEDOYED <mmax26072005@gmail.com>
2025-01-29 20:29:13 +0100

    udemy-js | 222

--- udemy-js/221/main.js
+++ udemy-js/221/main.js
@@ -10,3 +10,21 @@ const max = new Person('Max', 2005);
 console.log(max);
 
 console.log(max instanceof Person); // перевіряє чи є max екземпляром класу Person
+
+console.log('------------');
+
+///////////////////////////////////////////////////////////////////////////
+// Прототипи  Prototypes
+console.log(Person.prototype);
+
+Person.prototype.printAge = function () {
+  console.log(2025 - this.birthYear);
+};
+
+max.printAge();
+
+// вивід в консоль прототипа об'єкта
+
+console.log('------------');
+console.log(max.__proto__);
+console.log(max.__proto__ === Person.prototype);
